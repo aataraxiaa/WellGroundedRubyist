2d64b493840e9f04b85bca3cc1e2c8c8c88f6479
Pete Smith <peadar81@gmail.com>
2022-01-27 11:32:32 +0000

    Start Chapter 3

---
+++ ticket.rb
@@ -0,0 +1,57 @@
+class Ticket
+  def event
+    "Can't really be specified yet..."
+  end
+end
+
+ticketTwo = Ticket.new
+puts ticketTwo.event
+
+ticket = Object.new
+
+def ticket.date
+  "1903-01-02"
+end
+
+def ticket.venue
+  "Town Hall"
+end
+
+def ticket.event
+  "Author's reading"
+end
+
+def ticket.performer
+  "Mark Twain"
+end
+
+def ticket.seat
+  "Second Balcony, row j, seat 12"
+end
+
+def ticket.price
+  5.50
+end
+
+def ticket.available?
+  "pete"
+end
+
+def ticket.print_details(*x)
+  x.each { |detail| puts "This ticket is #{detail}" }
+end
+
+puts "This ticket is for: #{ticket.event}, at #{ticket.venue}, on #{ticket.date} .\n" +
+"The performer is #{ticket.performer} .\n" +
+"The seat is #{ticket.seat} , " +
+"and it costs $#{"%.2f" % ticket.price}"
+
+ticket.print_details('non-refundable.', 'non-transferable.', 'in a non-smoking section.')
+
+# print "Information desired: "
+# request = gets.chomp
+# if ticket.respond_to?(request)
+#   puts ticket.send(request)
+# else
+#   puts "No such information available"
+# end
