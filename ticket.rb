52dd7e8dbfc850cc89a79303e197ba511999c505
Pete Smith <peadar81@gmail.com>
2022-02-01 08:39:25 +0000

    Chapter 3

--- ticket.rb
+++ ticket.rb
@@ -1,57 +1,44 @@
 class Ticket
-  def event
-    "Can't really be specified yet..."
+  attr_reader :venue, :date, :price
+
+  def initialize(venue)
+    @venue = venue
+  end
+
+  def venue
+    @venue
+  end
+
+  def date
+    @date
+  end
+
+  def price=(amount)
+    @price = amount
+    "hi"
+  end
+
+  def price
+    @price
+  end
+
+  def discount(percent)
+    @price = @price * (100 - percent) / 100
+    "$#{"%.2f" % @price}."
+  end
+
+  def date=(date)
+    if !date.match(/\d{4}-\d{2}-\d{2}/)
+      puts "Please submit the date in the format 'yyyy-mm-dd'."
+      return
+    end
+
+    @date = date
   end
 end
 
-ticketTwo = Ticket.new
-puts ticketTwo.event
-
-ticket = Object.new
-
-def ticket.date
-  "1903-01-02"
-end
-
-def ticket.venue
-  "Town Hall"
-end
-
-def ticket.event
-  "Author's reading"
-end
-
-def ticket.performer
-  "Mark Twain"
-end
-
-def ticket.seat
-  "Second Balcony, row j, seat 12"
-end
-
-def ticket.price
-  5.50
-end
-
-def ticket.available?
-  "pete"
-end
-
-def ticket.print_details(*x)
-  x.each { |detail| puts "This ticket is #{detail}" }
-end
-
-puts "This ticket is for: #{ticket.event}, at #{ticket.venue}, on #{ticket.date} .\n" +
-"The performer is #{ticket.performer} .\n" +
-"The seat is #{ticket.seat} , " +
-"and it costs $#{"%.2f" % ticket.price}"
-
-ticket.print_details('non-refundable.', 'non-transferable.', 'in a non-smoking section.')
-
-# print "Information desired: "
-# request = gets.chomp
-# if ticket.respond_to?(request)
-#   puts ticket.send(request)
-# else
-#   puts "No such information available"
-# end
+th = Ticket.new("Town Hall")
+th.price = 100.00
+puts "The ticket for #{th.venue} has been discounted 15% to #{th.discount(15)}"
+th.date = "22-02-01"
+puts th.date = "2022-02-01"
