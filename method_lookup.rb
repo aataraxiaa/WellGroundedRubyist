09189c8e0bdd365dc1faabe62d9dbe0a110f70b7
Pete <peadar81@gmail.com>
2022-04-20 11:06:55 +0100

    Chapter 5

--- method_lookup.rb
+++ method_lookup.rb
@@ -5,11 +5,29 @@ module M
 end
 
 class C
-  include M
+  prepend M
+
+  def dmethod
+    puts "Hi 3"
+  end
 end
 
 class D < C
+  def dmethod
+    puts "Hi 1"
+  end
+
+  def dmethod
+    puts "Hi 4"
+  end
+
+  def dmethod
+    puts "Hi 2"
+  end
 end
 
 obj = D.new
 obj.report
+obj.dmethod
+
+puts D.ancestors
