890f608e1a326d6b27080f9d5633d609d1cc9fb5
Pete <peadar81@gmail.com>
2022-06-08 10:59:21 +0100

    Chapter 7 - Conversion, Boolean

--- chapter_7.rb
+++ chapter_7.rb
@@ -37,7 +37,7 @@ p s
 
 # Built-in to_* conversion methods
 
-# Every object, except `BasicObject`s responds to `to_s`
+# Every object, except `BasicObject`s, responds to `to_s`
 p ["one", "two"].to_s
 p Object.new.to_s
 
@@ -86,9 +86,83 @@ result = combine_names(*names)
 p result
 
 # Numerical conversion with `to_i` and `to_f`
-
 puts '1'.to_i
 puts 'hi'.to_i
 puts '123hi'.to_i
 puts 'hi123'.to_i
 puts 'h1e2l3l40'.to_i
+
+# Stricter conversions with Integer and Float
+# Integer('123a')
+puts Integer('123')
+
+# Role-playing to_* methods
+
+# String role-playing with to_str
+class Person
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+  def to_str
+    @name
+  end
+end
+person = Person.new('Pete', 21)
+combined = 'Hi ' + person
+puts combined
+
+# Array role-playing with to_ary
+class Person
+  def to_ary
+    [@name, @age]
+  end
+end
+
+array = ['Person']
+array << person
+puts array
+
+# Boolean states, Boolean objects, and nil
+
+# True and false as states
+if (class MyClass end)
+  puts "Empty class definition is true!"
+else
+  puts "Empty class definition is false!"
+end
+if (class MyClass; 1; end)
+  puts "Class definition with the number 1 in it is true!"
+else
+  puts "Class definition with the number 1 in it is false!"
+end
+if (def m; return false; end)
+  puts "Method definition is true!"
+else
+  puts "Method definition is false!"
+end
+if "string"
+  puts "Strings appear to be true!"
+else
+  puts "Strings appear to be false!"
+end
+if 100 > 50
+  puts "100 is greater than 50!"
+else
+  puts "100 is not greater than 50!"
+end
+
+# true and false as objects
+puts true.class
+puts true.class.superclass
+puts false.class
+puts false.class.superclass
+
+# The special object nil
+puts nil.class
+puts nil.class.superclass
+puts !!nil
+puts nil.to_i
+puts nil.object_id
+
+# Comparing two objects
